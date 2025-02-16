9f9d4dca6d5173f0cf5efdc40e270312f3ec00ed
hoatranh013 <chaunguyengiang2000@gmail.com>
2025-02-16 22:38:58 +0700

    update

--- AWSLambdaForTestingProject/obj/Debug/net7.0/AWSLambdaForTestingProject.AssemblyInfo.cs
+++ AWSLambdaForTestingProject/obj/Debug/net7.0/AWSLambdaForTestingProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSLambdaForTestingProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6059076096b155321884351fd7e9a743bd84898")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSLambdaForTestingProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSLambdaForTestingProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
